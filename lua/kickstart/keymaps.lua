6768da12475d52e6469d633a2fc9a88f36bc97f1
simoncra <csimonr28@gmail.com>
2025-05-16 15:54:44 -0400

    cambios para los keymaps de los buffers

--- lua/kickstart/keymaps.lua
+++ lua/kickstart/keymaps.lua
@@ -25,9 +25,9 @@ vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
 vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
 
 -- Buffers
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
-vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<S-x>", ":bdelete!<CR>", opts) -- close buffer
 
 -- Window management
 vim.keymap.set("n", "<leader>\\", "<C-w>v", opts) -- split window vertically
